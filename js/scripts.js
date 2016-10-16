8a0b009a76c29ad2df9abcb01128ebfbbe74b308
MadBander <lord1393@gmail.com>
2016-10-17 00:06:02 +0300

    xPacman update

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+$(document).on('click','.hw-trigger',function(e){
+  e.preventDefault();
+  $this = $(this);
+  $('.hover-window-bg').addClass('active')
+  $('.hover-window').removeClass('hover-window-show');
+
+  if(undefined != $this.data('width'))
+    $('.hover-window').css('width',$this.data('width'));
+  if(undefined != $this.data('height'))
+  $('.hover-window').css('height',$this.data('height'));
+
+  $('.hover-window .content').html($this.find('.content').html());
+  $('.hover-window').addClass('hover-window-show');
+});
+
+$(document).on('click','.hover-window-bg',function(){
+  $('.hover-window').removeClass('hover-window-show');
+  $('.hover-window2').removeClass('hover-window-show');
+  $(this).removeClass('active');
+});
+
+$(document).on('click','.hw-trigger2',function(e){
+  e.preventDefault();
+  $this = $(this);
+  $('.hover-window-bg').addClass('active')
+  $('.hover-window2').removeClass('hover-window-show');
+
+  if(undefined != $this.data('width'))
+  $('.hover-window2').css('width',$this.data('width'));
+  if(undefined != $this.data('height'))
+  $('.hover-window2').css('height',$this.data('height'));
+
+  $('.hover-window2').addClass('hover-window-show');
+});
+
+$(document).on('change','select[name="city"]',function(){
+  if ($(this).val() == 'trigger') {
+    $(this).after('<input type="text" name="city" placeholder="Ваш вариант" />');
+    $(this).remove();
+  }
+});
+
+$(document).on('change','.onclick',function(){
+  var uslugi = new Array();
+  $('.onclick:checked').each(function(i,e){
+    var usluga = $(e).data('title');
+    uslugi.push(usluga);
+  })
+
+  uslugi = uslugi.join(',');
+  $('.resultuslug').val(uslugi);
+
+  $result = parseInt($('.resultsum').val());
+  if ($(this).is(':checked')) {
+    var sum = $result + parseInt($(this).val());
+  } else {
+    var sum = $result - parseInt($(this).val());
+  }
+  $('.resultsum').val(sum);
+});
---
+++ send.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+require_once "mail.php";
+
+$myemail = 'ipacmanx@gmail.com'; //заменить на необходимый имейл
+
+$html;
+foreach ($_POST as $key => $value) {
+  $html .= '<b>'.$key.'</b>: '.$value.'<br/>';
+}
+
+  $mail = new Mail("admin@yandex.ru"); // E-mail адрес
+  $mail->setFromName("admin"); // Устанавливаем имя в обратном адресе
+  if (!$mail->send($myemail, "Новая заявка", "<h2>Новая заявка</h2>".$html)) echo "<h2>Неправильно заполнен email!</h2>";
+
+  echo "<h1>Спасибо!</h1>";
+
+?>
